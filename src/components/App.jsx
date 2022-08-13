0fd30ef0a633fa2fe9816ffd119d7bd4d7bd1f34
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-13 11:28:52 +0300

    add some

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { GlobalStyle } from "GlobalStyle";
+import { Box } from "Box";
+
+
+export class App extends Component  {
+  // Общий стейт в ближайшем предке 
+  static defaultProps = {
+    
+  }
+
+  state = {
+      
+      good: 0,
+      neutral: 0,
+      bad: 0
+  }
+
+  // Метод стейта
+
+  goodResultFeedback = () => {
+    this.setState ( prevState => ({
+      good: prevState.good +1,
+    
+    }));
+    console.log(this.state);
+    
+  }
+
+  neutralResultFeedback = () => {
+    this.setState ( prevState => ({
+      neutral: prevState.neutral +1,
+    
+    }));
+    console.log(this.state);
+  }
+
+  badResultFeedback = () => {
+    this.setState( prevState => ({
+      bad: prevState.bad +1,
+    }))
+    console.log(this.state);
+  }
+
+
+  //  Тотал 
+  countTotalFeedback = () => {
+    
+   }
+
+   // Процент 
+  countPositiveFeedbackPercentage = () => {
+   
+  }
+
+    render ()  {
+
+        
+    return (
+      <Box as='div'>
+        <>
+              <h3>Please leave feedback</h3>
+          
+              <button type="button" onClick={this.goodResultFeedback}>Good</button>
+              <button type="button" onClick={this.neutralResultFeedback}>Neutral</button>
+              <button type="button" onClick={this.badResultFeedback}>Bad</button>
+          </>
+
+              <h3>Statistics</h3>
+          <p>Good {this.state.good}</p>
+          <p>Neutral{this.state.neutral}</p>
+          <p>Bad{this.state.bad}</p>
+          <p>Total : {this.state.length}</p>
+          <p>Positive feedbacks</p>
+          <GlobalStyle />
+      </Box>
+    );
+    }
 };
